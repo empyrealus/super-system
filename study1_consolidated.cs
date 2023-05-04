38d8bf70c44d6acb08afa0d61b7ab6530a2ea9da
Empyrealus <103290689+empyrealus@users.noreply.github.com>
2023-05-03 19:19:25 -0500

    Create study1_consolidated.cs

---
+++ study1_consolidated.cs
@@ -0,0 +1,40 @@
+/*
+  study1 consolidate
+*/
+using System;
+using System.Collections.Generic;
+
+abstract class prototypes
+{
+    public abstract void do_ternary2<T>(ref T param);
+    public abstract string do_ternary(string x);
+}
+
+class Program<TXM> : prototypes // assign abstract prototypes to a generic class
+{
+	public TXM testingFunc<TXD>(TXM? value, TXD? param1){
+			 if(typeof(TXM) != null && param1 != null && typeof(TXD) != null){
+				  Console.WriteLine(value + " test 1");
+			    return value;
+			 }
+		     Program<string> x = new Program<string>();
+		     x.testingFunc<string>(null, null); 
+			 Console.WriteLine(value + " test 2");
+		     return value;
+			}
+     public TXM test { get => test; set{test =  testingFunc<string>(value, "string");}}
+	
+	 public void displayData(TXM data)
+	 {
+		 Console.WriteLine(this.test);
+	 }
+	 public override string do_ternary(string okay){
+	  return "String";
+	 }
+	public override void do_ternary2<T>(ref T test){}
+	
+	public static void Main()
+	{
+		Program<string> superProgram = new Program<string>();
+	}
+}
